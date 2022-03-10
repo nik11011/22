e708583823e0b9930a719bde21b5a2a0dee676a2
Bogdan1110 <101329012+Bogdan1110@users.noreply.github.com>
2022-03-10 15:46:20 +0300

    Add files via upload

---
+++ Check.cs
@@ -0,0 +1,17 @@
+﻿namespace УП_Практ_1
+{
+    class Check
+    {
+        public string name { get; set; }
+        public int idCheck { get; set; }
+        public double sum { get; set; }
+
+        public Check() { }
+        public Check(string  _name, int _idCheck, double _sum)
+        {
+            name = _name;
+            idCheck = _idCheck;
+            sum = _sum;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace УП_Практ_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string _name = Console.ReadLine();
+            int _idCheck = Convert.ToInt32(Console.ReadLine());
+            double sum = Convert.ToDouble(Console.ReadLine());
+            Check ch = new Check(_name, _idCheck, sum);
+            Console.WriteLine($"    {ch.name}     {ch.idCheck}     {ch.sum}");
+        }
+    }
+}
